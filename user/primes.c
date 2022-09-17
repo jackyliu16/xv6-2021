7fcc1f0363bbd728398a81c376d1f060b26c1f23
jackyliu <18922251299@163.com>
2022-09-17 17:36:37 +0800

    Fixed: primes
    	the error seem reuse of file prompts in different process.
    	modified:   user/primes.c

--- user/primes.c
+++ user/primes.c
@@ -3,6 +3,9 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+// DEBUG: 
+// 感觉是由于在进入第一个子进程的迭代过程中出现了由于子进程没有完成
+
 // int fork():              :create a process; if child return 0, else return getpid()
 // int exit(int status)     :Terminate current process, status report to wait();
 // int exec(char * file, char* argv[])
@@ -30,78 +33,106 @@ void print_array(int array[], int length){
     printf("\n");
 }
 
+// 参考https://blog.csdn.net/weixin_48283247/article/details/120602005
+// 文件描述符重定向(讲成映射比较好)
+void
+mapping(int n, int pd[])
+{
+  // 关闭文件描述符 n
+  close(n);
+  // 将管道的 读或写 端口复制到描述符 n 上
+  // 即产生一个 n 到 pd[n] 的映射
+  dup(pd[n]);
+  // 关闭管道中的描述符
+  close(pd[0]);
+  close(pd[1]);
+}
+
 // 还是有点不太理解pipe的作用机理，到底是将我们传入的那个数组作为指针指向了一个目标吗？
 // 还是说这个数组起到了其他的用途
+// 
 int main(int argc, char** argv){
-    int buf[35];
-    pipe(a2b);
-
+    int fd[2];
+    pipe(fd);
     if (fork() == 0) {
-        close(a2b[0]);
-        for ( int i = 1 ; i < 36 ; i++ ) {
-            write(a2b[1], &i, sizeof(int));
+        mapping(1, fd);
+        for ( int i = 2 ; i < 36 ; i++ ){ 
+            write(1, &i, sizeof(int));
         }
-        close(a2b[1]);
-        exit(0);
     }
     else {
         wait(NULL);
-        printf("fs: 1:%d, 2:%d", a2b[1], a2b[2]);
+        mapping(0, fd);
         recursion();
-        printf("back to main");
-        close(a2b[1]);
-        read(a2b[0], buf, sizeof(buf));
-        print_array(buf, sizeof(buf) / sizeof(int));
     }
     exit(0);
 }
 
-// 因为文件操作符是全局的，我感觉可以正常运行？如果说同时只有两个程序使用的话。
 void recursion() {
-    printf("fs: 1:%d, 2:%d", a2b[1], a2b[2]);
-    int buf[35];
+    int buf[35], i = 0;
+    while ( i < 35 ) {
+        buf[i] = 0;
+        i++;
+    }
 
-    close(a2b[1]);
-    read(a2b[0], buf, sizeof(buf));
-    close(a2b[0]);
-    int cnt = 0;
-    while ( buf[cnt] != 0 ) cnt++;
-    // only leaf one number
-    if ( cnt == 1 || cnt == 2 ) return;
+    int fd[2];
+    if ( read(0, &buf, sizeof(buf))) {
+        print_array(buf, sizeof(buf) / sizeof(int));
+        int cnt = 0 ;
+        while ( buf[cnt] != 0 && ++cnt < sizeof(buf) / sizeof(int) ) ;
 
-    printf("cnt: %d\n", cnt);
-    printf("turn: %d:", buf[0]);
-
-    print_array(buf, sizeof(buf) / sizeof(int));
-
-    if ( fork() == 0 ) {
-        printf("into child\n");
-        // child process
-        close(a2b[0]);
-        int i = buf[0] + 1;
-        write(a2b[1], &i, sizeof(int));
-        for ( int i = 1 ; i < cnt ; i++ ) {
-            if ( buf[i] % buf[0] != 0 ) {
-                // exact division
-                printf("divide: %d \n", buf[i]);
-                write(a2b[1], &buf[i], sizeof(int));
+        printf("cnt: %d\n", cnt);
+    
+        printf("primes %d\n", buf[0]);
+        
+        if ( cnt == 0 ) return;
+        pipe(fd);
+        if ( fork() == 0 ) {
+            mapping(1, fd);
+            for ( int i = 1 ; i < cnt ; i++ ) {
+                if ( buf[i] % buf[0] != 0 ) {
+                    int next = buf[i];
+                    write(1, &next, sizeof(int));
+                }
             }
         }
-        close(a2b[1]);
-
-        // try to read information
-        // int buf2[35];
-        // read(a2b[0], buf2, sizeof(buf2));
-        // print_array(buf2, sizeof(buf2) / sizeof(int));
-
-        // recursion();
-        exit(0);
-    }
-    else {
-        wait(NULL);
-        printf("into parent");
+        else {
+            wait(NULL);
+            mapping(0, fd);
+            recursion();
+        }
     }
 }
+
+// 因为文件操作符是全局的，我感觉可以正常运行？如果说同时只有两个程序使用的话。
+// void recursion() {
+//     printf("pid: {%d}\n", getpid());
+
+//     // read information
+//     int buf[35], i = 0;
+
+//     close(a2b[1]);
+//     read(a2b[0], &buf, sizeof(buf));
+//     print_array(buf, sizeof(buf) / sizeof(int));
+//     // account the current number
+//     int cnt = 0;    // the number right now
+//     while ( buf[++cnt] != 0 ) ;
+//     printf("cnt: %d\n", cnt);  
+
+//     // if 1 => error ; 2 => less one
+//     if ( cnt <= 2 ) return ;
+
+//     if (fork() == 0) {
+//         printf("into child process\n");
+//         close(a2b[1]);
+//         recursion();
+//         printf("back child process\n");
+//     }
+//     else {
+//         wait(NULL);
+//         exit(0);
+//     }
+// }
 
 // int main(int argc, char **argv) {
 //     int parent_fd[2];
